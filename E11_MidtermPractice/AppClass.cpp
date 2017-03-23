919b04e52a2377839c5f589f28edd16a35eb3f0b
pnwoodsum <pnw8434@ad.rit.edu>
2017-03-23 13:57:34 -0400

    Completed E11_MidtermPractice

--- E11_MidtermPractice/AppClass.cpp
+++ E11_MidtermPractice/AppClass.cpp
@@ -14,9 +14,37 @@ void AppClass::Update(void)
 	double dDeltaTime = m_pSystem->LapClock(); //time difference between function calls
 	dTotalTime += dDeltaTime; //Incrementing the time differences 
 #pragma endregion
+
 #pragma region YOUR CODE GOES HERE
 	m_m4Steve = glm::mat4(1.0f); // same as m_m4Steve = IDENTITY_M4; setting the identity to steve
+
+	static float fPercentage = 0.0f;
+	static vector3 v3CurrentPosition = vector3(0.0f, 0.0f, 0.0f);
+	static float fRotation = 0.0f;
+	static bool bOut = true;
+	static double dPercentTracker = 0;
+
+	dPercentTracker += dDeltaTime;
+
+	if (dPercentTracker > 5.0f) {
+		bOut = !bOut;
+		dPercentTracker = 0.0f;
+	}
+
+	fPercentage = MapValue((float)dPercentTracker, 0.0f, 5.0f, 0.0f, 1.0f);
+
+	if (bOut) {
+		v3CurrentPosition = glm::lerp(vector3(0.0f, 0.0f, 0.0f), vector3(0.0f, 5.0f, 0.0f), fPercentage);
+	}
+	else {
+		v3CurrentPosition = glm::lerp(vector3(0.0f, 5.0f, 0.0f), vector3(0.0f, 0.0f, 0.0f), fPercentage);
+	}
+
+	fRotation = glm::lerp(0.0f, 360.0f, fPercentage);
+
+	m_m4Steve = glm::rotate(m_m4Steve, fRotation, vector3(0.0f, 0.0f, 1.0f)) * glm::translate(v3CurrentPosition);
 #pragma endregion
+
 #pragma region DOES NOT NEED CHANGES
 	//Set the model matrix
 	m_pMeshMngr->SetModelMatrix(m_m4Steve, "Steve");
